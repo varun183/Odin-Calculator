3c4153b7f58cfb0044f9802b3131e3af3473eed9
varun183 <125751714+varun183@users.noreply.github.com>
2023-12-11 00:01:30 +0530

    write the functions for calculation and displaying

--- main.js
+++ main.js
@@ -1,49 +1,100 @@
-var firstNumber = "";
 var operator = "";
-var secondNumber="";
-var displayValue = "";
+var previousvalue = "";
+var currentValue = "";
 
 
 
-function add(a,b){
-    return a+b;
+var clear = document.querySelector("#clear-button");
+var equal = document.querySelector("#equal-button");
+var decimal = document.querySelector("#decimal-button");
+
+var numbers = document.querySelectorAll(".number");
+var operators = document.querySelectorAll(".operator");
+
+var previousScreen = document.querySelector(".previous");
+var currentScreen = document.querySelector(".current");
+
+
+numbers.forEach((number)=>number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent)
+    currentScreen.textContent = currentValue;
+
+}))
+
+operators.forEach((op)=> op.addEventListener("click", function(e){
+    handleOperator(e.target.textContent)
+    previousScreen.textContent = previousvalue + " " + operator;
+    currentScreen.textContent = currentValue;
+
+}))
+
+clear.addEventListener("click",function(){
+    previousvalue = "";
+    currentValue = "";
+    operator = "";
+    previousScreen.textContent = currentValue;
+    currentScreen.textContent = currentValue;
+})
+
+equal.addEventListener("click", function(){
+
+    if(currentValue!="" && previousvalue!=""){
+        calculate()
+        previousScreen.textContent = "";
+
+        
+        currentScreen.textContent = previousvalue;
+        
+    }
+})
+
+
+decimal.addEventListener("click",function(){
+    addDecimal();
+})
+
+function handleNumber(num){
+    if(num.length<=6)
+     currentValue +=num;
 }
 
-function substract(a,b){
-    return a-b;
+function handleOperator(op){
+    console.log(op)
+    operator = op;
+    previousvalue=currentValue;
+    currentValue="";
+
 }
 
-function multiply(a,b){
-    return a*b;
-}
+function calculate(){
+    previousvalue = Number(previousvalue);
+    currentValue = Number(currentValue);
 
-function divide(a,b){
-    return a/b;
-}
-
-
-function operate(operator,firstNumber,secondNumber){
-
-    a=parseFloat(firstNumber);
-    b=parseFloat(secondNumber);
-
-    switch (operator) {
-        case "+":    
-           return add( a,b );
-        case "-":
-            return substract(a,b);
-        case "*":
-            return multiply(a,b);
-        case "/":
-            return divide(a,b);
-        default:
-            return "Error!";
+    if(operator==="+"){
+        previousvalue +=currentValue;
+    }else if(operator==="-"){
+        previousvalue -=currentValue;
+    }else if(operator==="x"){
+        previousvalue *= currentValue;
+    }else{
+        previousvalue/=currentValue;
     }
 
-    
+    previousvalue =  roundNumber(previousvalue);
+
+    previousvalue = previousvalue.toString();
+    currentValue = previousvalue.toString();
+
 }
 
-function updateDisplay(){
-    document.getElementById("displayPanel").textContent=displayValue;
+function roundNumber(num){
+    return Math.round(num*1000)/1000;
 }
 
+function addDecimal(){
+
+    if(!currentValue.includes(".")){
+        currentValue +=".";
+    }
+
+}
